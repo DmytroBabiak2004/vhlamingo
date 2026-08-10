775f192b81136e1e6f69c29a6ff59b3795d28418
Dmytro Babiak <dimababjak2004@gmail.com>
2026-08-11 00:28:05 +0300

    ssdf

--- src/components/FlipCard.tsx
+++ src/components/FlipCard.tsx
@@ -121,8 +121,8 @@ export function FlipCard({ card, isFlipped, onFlip, hapticsEnabled, maxHeight }:
                 <Image
                   source={require("@/assets/images/flamingo.png")}
                   style={{
-                    width: cf(250),
-                    height: cf(250),
+                    width: cf(220),
+                    height: cf(220),
                   }}
                   resizeMode="contain"
                 />
